7585c9ad54baaf088c86155277bb656826818bcd
Khaela Fortunela <72397657+fortunelagit@users.noreply.github.com>
2022-06-26 15:24:31 +0700

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+fetch("https://www.kpopmap.com/wp-admin/admin-ajax.php", {
+  "headers": {
+	"accept": "*/*",
+	"accept-language": "en-US,en;q=0.9",
+	"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+	"sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"",
+	"sec-ch-ua-mobile": "?1",
+	"sec-ch-ua-platform": "\"Android\"",
+	"sec-fetch-dest": "empty",
+	"sec-fetch-mode": "cors",
+	"sec-fetch-site": "same-origin",
+	"x-requested-with": "XMLHttpRequest",
+	"cookie": "usprivacy=1---; kpopmap_uid=858e011428510a8452f41684541c55c2; PHPSESSID=22jc857idtqko5h3kkc15dk6ns; _pw_fingerprint=%2242d4ba882febc61cb45285bed1e0f4b8%22; _ga=GA1.1.174979198.1656194617; udmsrc=%7B%7D; _pbjs_userid_consent_data=3524755945110770; trc_cookie_storage=taboola%2520global%253Auser-id%3D96148e11-7c24-4237-8672-4b06495a97e7-tuct9b10bc8; __gpi=UID=000006f7eeb2e1ad:T=1656194640:RT=1656194640:S=ALNI_MbDTO_3Rh1awLMJ7UX7laEP9Ox4oQ; __qca=P0-245149130-1656194642673; __gads=ID=0c339a37b5676d92-22fdf650cad400fa:T=1656194640:S=ALNI_MZEHfbpmR8F9Dv7AmdlADjhcX_gxA; FCNEC=[[\"AKsRol_A_fyqqFSKAcRHO4-WPLrjqsYloRujF3Akkdf629E6-OSat1XdGxn1ErxlNRelaAG1vjs7mYgo_yrKeHEhoy9hCVMScIKQIfB3CwcwiN0Px8hV2ZtcUDgk77n9mxom8CM2DCDXwvptjXE-HrudcdpqctQHtg==\"],null,[]]; pbjs-unifiedid=%7B%22TDID%22%3A%223b2eb796-d51b-4830-9826-d1773091d7d5%22%2C%22TDID_LOOKUP%22%3A%22FALSE%22%2C%22TDID_CREATED_AT%22%3A%222022-06-26T03%3A08%3A48%22%7D; _clck=ad1yo7|1|f2n|0; udm_session_rad=1; _clsk=1fpyzk5|1656212937373|1|1|e.clarity.ms/collect; panoramaId_expiry=1656299368710; udm_session=5; udm_edge_floater_fcap=%5B1656212977197%2C1656212972116%2C1656212966999%2C1656212934743%2C1656212926136%2C1656194633893%5D; cto_bundle=3cDbe19aM2k4YzEwbiUyRnllQjdRWHA1amxNWE1mUCUyQnY5aFBvVm5wWkxyZjB4OVU2dXZ1Q092b09MTkFaMiUyRnlKUG9mN2VHZzR3Tm5RWWdNUEZCRE1lbFE3Y1NobDVsMiUyQlV4bVZoWjFLS0dzOHhYOHVvUFNaMlQ5VTZ5aHJvaGxHYnNOSVl3; _ga_7D5PYN54PQ=GS1.1.1656212928.3.1.1656213033.57; AWSALB=uNtbFEdD6H3nf8o8IDmWjifMof6rkwDqsykTrRs6dkrCb+Gq7tIyHQlHIHss1a+kTEwmjg7e3jRfFno8kmmFUeMDg7iGnEId7z5ByZ1TUkhmcXyrM8PHJXitpSQd; AWSALBCORS=uNtbFEdD6H3nf8o8IDmWjifMof6rkwDqsykTrRs6dkrCb+Gq7tIyHQlHIHss1a+kTEwmjg7e3jRfFno8kmmFUeMDg7iGnEId7z5ByZ1TUkhmcXyrM8PHJXitpSQd",
+	"Referer": "https://www.kpopmap.com/which-k-pop-male-idol-makes-perfect-use-of-social-media-by-sharing-boyfriend-material-pictures-with-fans/",
+	"Referrer-Policy": "unsafe-url"
+  },
+  "body": "action=kpopmapQuizAjax&mode=QuizResultSave&sid=8042&quiz_type=B_ver2&nonce=0e843fb576&n=1539&login_gubun=N",
+  "method": "POST"
+});
+ 
+
+
+
+
+ 
+
+
+
